bd7a34bb942cbe14a55b1a88b0ea17ad3a2273be
Jason Ma <jasonma@gmail.com>
2016-03-12 20:21:57 -0800

    add boxScore

--- formatJSON/boxScoreReformat.py
+++ formatJSON/boxScoreReformat.py
@@ -3,69 +3,140 @@ import pprint
 import pymongo
 
 
-client = pymongo.MongoClient("mongodb://localhost:27017")
-db = client.nba
-#db = client.test
+#client = pymongo.MongoClient("mongodb://localhost:27017")
+#db = client.nba
 
-def agg1():
-    result = db.sigOptBox.aggregate([
-    {"$match":{"resultSets.headers":{"$in":["GAME_ID",
-						"TEAM_ID",
-						"TEAM_NAME",
-						"TEAM_ABBREVIATION",
-						"TEAM_CITY",
-						"MIN",
-						"FGM",
-						"FGA",
-						"FG_PCT",
-						"FG3M",
-						"FG3A",
-						"FG3_PCT",
-						"FTM",
-						"FTA",
-						"FT_PCT",
-						"OREB",
-						"DREB",
-						"REB",
-						"AST",
-						"STL"
-							]
-					  
-			}
-		}
-    }, 
+# Connection attributes
+hostname = 'localhost'
+port = 27017
+database = 'nba'
+
+# Connect to DB
+client = pymongo.MongoClient(hostname,port)
+db = client[database]
+
+
+# Function will reformat JSON into more readable format and store into playerBoxScore collection
+def playerCollectionReformat():
+    result = db.orig.aggregate([
     {"$project":{
-		"resultSets":{"$arrayElemAt":["$resultSets", 5]}
+                "resultSets1":{"$arrayElemAt":["$resultSets", 0]},
+                "resultSets2":{"$arrayElemAt":["$resultSets", 4]}
+                }
+    },
+    {"$unwind":"$resultSets1.rowSet"},
+    {"$unwind":"$resultSets2.rowSet"},
+    {"$project":{
+                "_id":0,
+                "DATE":{"$arrayElemAt":["$resultSets1.rowSet",0]},
+                "HOME_TEAM_ID":{"$arrayElemAt":["$resultSets1.rowSet",6]},
+                "SEASON":{"$arrayElemAt":["$resultSets1.rowSet",8]},
+                "GAME_ID":{"$arrayElemAt":["$resultSets2.rowSet",0]},
+                "TEAM_ID":{"$arrayElemAt":["$resultSets2.rowSet",1]},
+                "TEAM_ABBREVIATION":{"$arrayElemAt":["$resultSets2.rowSet",3]},
+                "PLAYER_ID":{"$arrayElemAt":["$resultSets2.rowSet",4]},
+                "PLAYER_NAME":{"$arrayElemAt":["$resultSets2.rowSet",5]},
+                "START_POSITION":{"$arrayElemAt":["$resultSets2.rowSet",6]},
+                "MIN":{"$arrayElemAt":["$resultSets2.rowSet",8]},
+                "FGM":{"$arrayElemAt":["$resultSets2.rowSet",9]},
+                "FGA":{"$arrayElemAt":["$resultSets2.rowSet",10]},
+                "FG_PCT":{"$arrayElemAt":["$resultSets2.rowSet",11]},
+                "FG3M":{"$arrayElemAt":["$resultSets2.rowSet",12]},
+                "FG3A":{"$arrayElemAt":["$resultSets2.rowSet",13]},
+                "FG3_PCT":{"$arrayElemAt":["$resultSets2.rowSet",14]},
+                "FTM":{"$arrayElemAt":["$resultSets2.rowSet",15]},
+                "FTA":{"$arrayElemAt":["$resultSets2.rowSet",16]},
+                "FT_PCT":{"$arrayElemAt":["$resultSets2.rowSet",17]},
+                "OREB":{"$arrayElemAt":["$resultSets2.rowSet",18]},
+                "DREB":{"$arrayElemAt":["$resultSets2.rowSet",19]},
+                "REB":{"$arrayElemAt":["$resultSets2.rowSet",20]},
+                "AST":{"$arrayElemAt":["$resultSets2.rowSet",21]},
+                "STL":{"$arrayElemAt":["$resultSets2.rowSet",22]},
+                "BLK":{"$arrayElemAt":["$resultSets2.rowSet",23]},
+                "TO":{"$arrayElemAt":["$resultSets2.rowSet",24]},
+                "PF":{"$arrayElemAt":["$resultSets2.rowSet",25]},
+                "PTS":{"$arrayElemAt":["$resultSets2.rowSet",26]},
+                "PLUS_MINUS":{"$arrayElemAt":["$resultSets2.rowSet",27]},
+                 }
+    },
+    {"$out":"test"}
+   ])
+
+
+# Function will reformat JSON into more readable format and store into another collection
+def boxScoreReformat():
+    result = db.sigopt.aggregate([
+    {"$project":{
+		"resultSets1":{"$arrayElemAt":["$resultSets", 0]},
+		"resultSets2":{"$arrayElemAt":["$resultSets", 5]}
 		}
-    }	,						
-    {"$unwind":"$resultSets.rowSet"},
+    },						
+    {"$unwind":"$resultSets1.rowSet"},
+    {"$unwind":"$resultSets2.rowSet"},
     {"$project":{ 
 		"_id":0,
-		"GAME_ID":{"$arrayElemAt":["$resultSets.rowSet",0]}, 
-		"TEAM_ID":{"$arrayElemAt":["$resultSets.rowSet",1]}, 
-		"TEAM_NAME":{"$arrayElemAt":["$resultSets.rowSet",2]}, 
-		"TEAM_ABBREVIATION":{"$arrayElemAt":["$resultSets.rowSet",3]}, 
-		"TEAM_CITY":{"$arrayElemAt":["$resultSets.rowSet",4]}, 
-		"MIN":{"$arrayElemAt":["$resultSets.rowSet",5]}, 
-		"FGM":{"$arrayElemAt":["$resultSets.rowSet",6]}, 
-		"FGA":{"$arrayElemAt":["$resultSets.rowSet",7]}, 
-		"FG_PCT":{"$arrayElemAt":["$resultSets.rowSet",8]}, 
-		"FG3M":{"$arrayElemAt":["$resultSets.rowSet",9]}, 
-		"FG3A":{"$arrayElemAt":["$resultSets.rowSet",10]}, 
-		"FG3_PCT":{"$arrayElemAt":["$resultSets.rowSet",11]}, 
-		"FTM":{"$arrayElemAt":["$resultSets.rowSet",12]}, 
-		"FTA":{"$arrayElemAt":["$resultSets.rowSet",13]}, 
-		"FT_PCT":{"$arrayElemAt":["$resultSets.rowSet",14]}, 
-		"OREB":{"$arrayElemAt":["$resultSets.rowSet",15]}, 
-		"DREB":{"$arrayElemAt":["$resultSets.rowSet",16]}, 
-		"REB":{"$arrayElemAt":["$resultSets.rowSet",17]}, 
-		"AST":{"$arrayElemAt":["$resultSets.rowSet",18]}, 
-		"STL":{"$arrayElemAt":["$resultSets.rowSet",19]}, 
-		"BLK":{"$arrayElemAt":["$resultSets.rowSet",20]}, 
-		"TO":{"$arrayElemAt":["$resultSets.rowSet",21]}, 
-		"PF":{"$arrayElemAt":["$resultSets.rowSet",22]}, 
-		"PTS":{"$arrayElemAt":["$resultSets.rowSet",23]}, 
-		"PLUS_MINUS":{"$arrayElemAt":["$resultSets.rowSet",24]}, 
+		"DATE":{"$arrayElemAt":["$resultSets1.rowSet",0]}, 
+		"HOME_TEAM_ID":{"$arrayElemAt":["$resultSets1.rowSet",6]}, 
+		"SEASON":{"$arrayElemAt":["$resultSets1.rowSet",8]}, 
+		"GAME_ID":{"$arrayElemAt":["$resultSets2.rowSet",0]}, 
+		"TEAM_ID":{"$arrayElemAt":["$resultSets2.rowSet",1]}, 
+		"TEAM_NAME":{"$arrayElemAt":["$resultSets2.rowSet",2]}, 
+		"TEAM_ABBREVIATION":{"$arrayElemAt":["$resultSets2.rowSet",3]}, 
+		"TEAM_CITY":{"$arrayElemAt":["$resultSets2.rowSet",4]}, 
+		"MIN":{"$arrayElemAt":["$resultSets2.rowSet",5]}, 
+		"FGM":{"$arrayElemAt":["$resultSets2.rowSet",6]}, 
+		"FGA":{"$arrayElemAt":["$resultSets2.rowSet",7]}, 
+		"FG_PCT":{"$arrayElemAt":["$resultSets2.rowSet",8]}, 
+		"FG3M":{"$arrayElemAt":["$resultSets2.rowSet",9]}, 
+		"FG3A":{"$arrayElemAt":["$resultSets2.rowSet",10]}, 
+		"FG3_PCT":{"$arrayElemAt":["$resultSets2.rowSet",11]}, 
+		"FTM":{"$arrayElemAt":["$resultSets2.rowSet",12]}, 
+		"FTA":{"$arrayElemAt":["$resultSets2.rowSet",13]}, 
+		"FT_PCT":{"$arrayElemAt":["$resultSets2.rowSet",14]}, 
+		"OREB":{"$arrayElemAt":["$resultSets2.rowSet",15]}, 
+		"DREB":{"$arrayElemAt":["$resultSets2.rowSet",16]}, 
+		"REB":{"$arrayElemAt":["$resultSets2.rowSet",17]}, 
+		"AST":{"$arrayElemAt":["$resultSets2.rowSet",18]}, 
+		"STL":{"$arrayElemAt":["$resultSets2.rowSet",19]}, 
+		"BLK":{"$arrayElemAt":["$resultSets2.rowSet",20]}, 
+		"TO":{"$arrayElemAt":["$resultSets2.rowSet",21]}, 
+		"PF":{"$arrayElemAt":["$resultSets2.rowSet",22]}, 
+		"PTS":{"$arrayElemAt":["$resultSets2.rowSet",23]}, 
+		"PLUS_MINUS":{"$arrayElemAt":["$resultSets2.rowSet",24]}, 
+		 } 
+    },
+    {"$project":{ 
+		"DATE":1,
+		"SEASON":1,
+		"GAME_ID":1,
+		"TEAM_ID":1, 
+		"TEAM_NAME":1, 
+		"TEAM_ABBREVIATION":1, 
+		"TEAM_CITY":1, 
+		"MIN":1, 
+		"FGM":1, 
+		"FGA":1, 
+		"FG_PCT":1, 
+		"FG3M":1, 
+		"FG3A":1, 
+		"FG3_PCT":1, 
+		"FTM":1, 
+		"FTA":1, 
+		"FT_PCT":1, 
+		"OREB":1, 
+		"DREB":1, 
+		"REB":1, 
+		"AST":1, 
+		"STL":1, 
+		"BLK":1, 
+		"TO":1, 
+		"PF":1, 
+		"PTS":1, 
+		"PLUS_MINUS":1, 
+		"WHERE":{
+		   "$cond": {"if": {"$eq" :["$TEAM_ID","$HOME_TEAM_ID"] }, "then":"HOME", "else":"AWAY"}
+			},
+		"PTSA":{"$subtract":["$PTS","$PLUS_MINUS"]}
 		 } 
     },
     {"$out":"test"}
@@ -74,9 +145,9 @@ def agg1():
     return result
 
 if __name__ == '__main__':
-	result1 = agg1()
+	result1 = boxScoreReformat()
 	for document in result1:
 		print(document)
-                exit()
+        #       exit()
 
 
